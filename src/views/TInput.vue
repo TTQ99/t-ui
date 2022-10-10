72acc08c65d73257ea1119daa1dcc5c8248c484f
ttq <ttq99@qq.com>
2022-10-10 23:05:09 +0800

    feat: create tinput components

---
+++ src/views/TInput.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <input :type="type" :value="value" @input="onInput" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TInput",
+  props: {
+    type: {
+      type: String,
+      default: "text"
+    },
+    value: {
+      type: String,
+      default: ""
+    }
+  },
+  methods:{
+      onInput(e){
+          this.$emit('input', e.target.value)
+      }
+  }
+};
+</script>
+
+<style>
+</style>
